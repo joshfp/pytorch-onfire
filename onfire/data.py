82702fcc1fc0265dce1aa23dfcd84c5d43fad0de
Jose Fernandez Portal <jose.fp@gmail.com>
2020-08-23 12:24:09 -0300

    avoid moving tensors to cuda inside dataloaders

--- onfire/data.py
+++ onfire/data.py
@@ -10,32 +10,18 @@ __all__ = [
 ]
 
 class OnFireDataLoader(DataLoader):
-    def __init__(self, data, tfms, batch_size, shuffle=False, num_workers=-1, device=None,
-                 sampler=None, pin_memory=False, drop_last=False, **kwargs):
+    def __init__(self, data, tfms, batch_size, shuffle=False, num_workers=-1,
+                 sampler=None, pin_memory=None, drop_last=False, **kwargs):
         self.ds = OnFireDataset(data)
         self.tfms = tfms
         num_workers = num_workers if num_workers >= 0 else os.cpu_count()
-        self.device = device or ('cuda' if torch.cuda.is_available() else 'cpu')
-
+        pin_memory = pin_memory or torch.cuda.is_available()
         super().__init__(self.ds, batch_size=batch_size, shuffle=shuffle,
                          num_workers=num_workers, collate_fn=self.__collate, sampler=sampler,
                          pin_memory=pin_memory, drop_last=drop_last, **kwargs)
 
-    def __iter__(self):
-        for batch in super().__iter__():
-            yield self.__all_tensors_to_device(batch)
-
     def __collate(self, batch):
             return tuple([tfm(batch) for tfm in self.tfms])
-
-    def __all_tensors_to_device(self, X):
-            if isinstance(X, torch.Tensor):
-                return X.to(self.device)
-            elif isinstance(X, (list, tuple)):
-                res = [self.__all_tensors_to_device(x) for x in X]
-                return res if isinstance(X, list) else tuple(res)
-            elif isinstance(X, dict):
-                return {k: self.__all_tensors_to_device(v) for k,v in X.items()}
 
 
 class OnFireDataset(Dataset):
--- onfire/utils.py
+++ onfire/utils.py
@@ -1,7 +1,9 @@
 from functools import wraps
+import torch
 
 __all__ = [
     'mappify',
+    'batch_to_device',
 ]
 
 def mappify(func):
@@ -9,3 +11,13 @@ def mappify(func):
     def inner(X, **kwargs):
         return [func(x, **kwargs) for x in X]
     return inner
+
+
+def batch_to_device(batch, device):
+    if isinstance(batch, torch.Tensor):
+        return batch.to(device)
+    elif isinstance(batch, (list, tuple)):
+        res = [batch_to_device(x, device) for x in batch]
+        return res if isinstance(batch, list) else tuple(res)
+    elif isinstance(batch, dict):
+        return {k: batch_to_device(v, device) for k,v in batch.items()}
