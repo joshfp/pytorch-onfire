6363dcdc0b5299f9a407b1d52987cdb9156d1d67
fededosreis <fededosreis@gmail.com>
2022-07-27 10:35:30 -0300

    map size config + ProjectorFeature

--- onfire/data.py
+++ onfire/data.py
@@ -14,10 +14,11 @@ __all__ = [
 
 class OnFireDataLoader(DataLoader):
     def __init__(self, data, tfms, batch_size, shuffle=False, num_workers=0,
-                 sampler=None, pin_memory=None, drop_last=False, **kwargs):
+                 sampler=None, pin_memory=None, drop_last=False, map_size:int=1024**4,
+                 **kwargs):
         num_workers = num_workers if num_workers else os.cpu_count()
         pin_memory = pin_memory if pin_memory != None else torch.cuda.is_available()
-        self.ds = OnFireDataset(data, max_readers=num_workers)
+        self.ds = OnFireDataset(data, max_readers=num_workers, map_size = map_size)
         self.tfms = tfms if isinstance(tfms, (list, tuple)) else [tfms]
         super().__init__(self.ds, batch_size=batch_size, shuffle=shuffle,
                          num_workers=num_workers, collate_fn=self.__collate, sampler=sampler,
@@ -28,11 +29,11 @@ class OnFireDataLoader(DataLoader):
 
 
 class OnFireDataset(Dataset):
-    def __init__(self, data, max_readers):
+    def __init__(self, data, max_readers, map_size:int=1024**4):
         self.use_lmdb = max_readers > 1
         if self.use_lmdb:
             tmpdir = tempfile.TemporaryDirectory().name
-            self.db = lmdb.open(tmpdir, map_size=1024**4, lock=False, max_readers=max_readers)
+            self.db = lmdb.open(tmpdir, map_size=map_size, lock=False, max_readers=max_readers)
             self.key_struct = struct.Struct("!q")
             it = [(self.key_struct.pack(i), msgpack.packb(x)) for i, x in enumerate(data)]
             with self.db.begin(write=True) as txn:
